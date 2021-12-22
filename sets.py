c5e51fc972282f45cd7f229b08b1eeeb4284c1dc
ISMAILSHARIEFB <Ismailsharief.Babu@mindtree.com>
2021-12-22 12:06:33 +0530

    tuples and sets

---
+++ sets.py
@@ -0,0 +1,16 @@
+#set
+s = set()
+
+print(type(s))
+s.add(1)
+s.add('a')
+s.add('[1, 3, 7]')
+
+print(s)
+print(len(s))
+
+#list
+my_list = ['a', 'c', 'a', 'b', 'a', 'c', 1, 4, 3, 1]
+
+#cast set to list
+print(set(my_list))
---
+++ tuples.py
@@ -0,0 +1,14 @@
+#tuple - Immutable as String
+tup = ('a', 3, 0.35, [1, 2])
+
+#list
+my_list = ['a', 3, 90.808]
+
+print(tup)
+print(type(tup))
+print(type(my_list))
+print(tup.count('a'))
+print(tup.index(3))
+
+my_list.append('lklkl')
+print(my_list)
